83d014d20ef01ab5e624e6937353fc42274a86cb
rooney <bch3454@gmail.com>
2023-06-10 20:19:06 +0900

    Feat: 6.3 fromJson

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonfix/models/webtoon_model.dart';
 
 class ApiServices {
+  List<WebtoonModel> webtoonInstance = [];
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = 'today';
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        webtoonInstance.add(toon);
+      }
+      return webtoonInstance;
     }
     throw Error();
   }
